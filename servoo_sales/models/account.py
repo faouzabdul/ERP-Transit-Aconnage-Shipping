877c285e43d8f4dcfc06d391f7886cc1fe17c19f
myenke <yenkus@gmail.com>
2022-12-09 12:02:26 +0100

    Prise en compte des recommandations suite seance de travail avec la facturation APM:  - Intégration de l'unité du poids  - Traduction des termes  - Correction du titre du document sur la facture  - Ajout du montant en lettre sur la proforma  - Ajout du montant en FCA pour les factures et proforma qui sont faite en euro et vice versa  - Affichage d'un seul montant en lettre sur les factures faites en euro  - Augmentation de la taille de la police  - Afficher le taux sur la facture et la proforma

--- servoo_sales/models/account.py
+++ servoo_sales/models/account.py
@@ -14,7 +14,8 @@ class AccountMode(models.Model):
     unloading_place_id = fields.Many2one('res.locode', string='Unloading place')
     transport_letter = fields.Char('N° BL / N° LTA', index=True)
     volume = fields.Float('Volume (m3)', digits=(12, 3))
-    weight = fields.Float('Weight (Kg)', digits=(12, 3))
+    weight = fields.Float('Weight', digits=(12, 3))
+    unit_id = fields.Many2one('res.unit', 'Unit')
     custom_declaration_reference = fields.Char('Custom Declaration Reference')
     custom_declaration_date = fields.Date('Custom Declaration Date')
     assessed_value = fields.Float('Assessed Value', digits=(12, 3), help='Valeur imposable')
@@ -25,8 +26,19 @@ class AccountMode(models.Model):
     amount_total_in_currency_signed_letter = fields.Char('Total in Currency Signed letter', compute='_compute_display_amount_letter',
                                                store=False)
 
-    @api.depends('amount_total')
+    other_currency_id = fields.Many2one('res.currency', 'Other Currency')
+    amount_other_currency = fields.Float(string='Total Currency', store=True, digits=(6, 3),
+                                         compute='_compute_display_amount_letter')
+
+    @api.depends('amount_total', 'currency_id')
     def _compute_display_amount_letter(self):
         for move in self:
             move.amount_total_signed_letter = utils.translate(move.amount_total_signed).upper()
             move.amount_total_in_currency_signed_letter = utils.translate(move.amount_total_in_currency_signed).upper()
+            currency_code = 'XAF'
+            if move.currency_id.name == 'XAF':
+                currency_code = 'EUR'
+            other_currency = self.env['res.currency'].search([('name', '=', currency_code)])
+            rate = other_currency.rate if currency_code == 'EUR' else other_currency.inverse_rate
+            move.other_currency_id = other_currency and other_currency.id
+            move.amount_other_currency = move.amount_total_signed * rate
--- servoo_sales/models/sale_order.py
+++ servoo_sales/models/sale_order.py
@@ -2,6 +2,7 @@
 # Author: Marius YENKE MBEUYO
 
 from odoo import models, fields, api, _
+from . import utils
 
 
 class SaleOrder(models.Model):
@@ -13,12 +14,30 @@ class SaleOrder(models.Model):
     unloading_place_id = fields.Many2one('res.locode', string='Unloading place')
     transport_letter = fields.Char('N° BL / N° LTA', index=True)
     volume = fields.Float('Volume (m3)', digits=(12, 3))
-    weight = fields.Float('Weight (Kg)', digits=(12, 3))
+    weight = fields.Float('Weight', digits=(12, 3))
+    unit_id = fields.Many2one('res.unit', 'Unit')
     custom_declaration_reference = fields.Char('Custom Declaration Reference')
     custom_declaration_date = fields.Date('Custom Declaration Date')
     assessed_value = fields.Float('Assessed Value', digits=(12, 3), help='Valeur imposable')
     object = fields.Text('Object')
     number_of_packages = fields.Char('Number of packages/TC')
+    amount_total_letter = fields.Char('Total letter', compute='_compute_display_amount_letter',
+                                      store=False)
+    other_currency_id = fields.Many2one('res.currency', 'Other Currency')
+    amount_other_currency = fields.Float(string='Total Currency', store=True, digits=(6, 3),
+                                         compute='_compute_display_amount_letter')
+
+    @api.depends('amount_total', 'currency_id')
+    def _compute_display_amount_letter(self):
+        for order in self:
+            order.amount_total_letter = utils.translate(order.amount_total).upper()
+            currency_code = 'XAF'
+            if order.currency_id.name == 'XAF':
+                currency_code = 'EUR'
+            other_currency = self.env['res.currency'].search([('name', '=', currency_code)])
+            rate = other_currency.rate if currency_code == 'EUR' else other_currency.inverse_rate
+            order.other_currency_id = other_currency and other_currency.id
+            order.amount_other_currency = order.amount_total * rate
 
     def _create_invoices(self, grouped=False, final=False, date=None):
         moves = super(SaleOrder, self)._create_invoices(grouped=grouped, final=final, date=date)
